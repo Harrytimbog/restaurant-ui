94381c52cb874523bb3f34adb27b924d65dd8863
Timilehin Arigbede <Harriemannie@gmail.com>
2022-02-10 20:39:33 +0100

    Navbar design and Nextjs slider

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,10 +1,60 @@
 import Image from 'next/image';
 import styles from '../styles/Featured.module.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 const Featured = () => {
-  return <div>Featured</div>;
+  const [index, setIndex] = useState(0);
+
+  const images = ['/image/featured2.png', '/image/featured3.png'];
+
+  const handleArrow = (direction) => {
+    if (direction === 'l') {
+      setIndex(index !== 0 ? index - 1 : 1);
+    }
+    if (direction === 'r') {
+      setIndex(index !== 1 ? index + 1 : 0);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.arrowContainer}
+        style={{ left: 0 }}
+        onClick={() => handleArrow('l')}
+      >
+        <Image
+          src="/image/arrowl.png"
+          alt=""
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
+      <div
+        className={styles.wrapper}
+        style={{ transform: `translateX(${-100 * index}vw)` }}
+      >
+        {images.map((img, i) => (
+          <div className={styles.imgContainer} key={i}>
+            <Image src={img} alt="" layout="fill" objectFit="contain" />
+          </div>
+        ))}
+      </div>
+      <div
+        className={styles.arrowContainer}
+        style={{ right: 0 }}
+        onClick={() => handleArrow('r')}
+      >
+        <Image
+          src="/image/arrowr.png"
+          alt=""
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
