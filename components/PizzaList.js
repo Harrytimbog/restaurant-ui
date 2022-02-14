18bbd5a97eee3ef1e2b303aa5b1b20720599c093
Timilehin Arigbede <Harriemannie@gmail.com>
2022-02-14 16:38:12 +0100

    Mobile first design

--- components/PizzaList.js
+++ components/PizzaList.js
@@ -4,7 +4,7 @@ import PizzaCard from './PizzaCard';
 const PizzaList = () => {
   return (
     <div className={styles.container}>
-      <h1>THE BEST PIZZA IN TOWN</h1>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
       <p className={styles.desc}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -17,7 +17,7 @@ const Product = () => {
     <div className={styles.container}>
       <div className={styles.left}>
         <div className={styles.imgContainer}>
-          <Image src={pizza.img} objectFit="contain" alt="" layout="fill" />
+          <Image src={pizza.img} objectFit="contain" layout="fill" alt="" />
         </div>
       </div>
       <div className={styles.right}>
@@ -27,15 +27,15 @@ const Product = () => {
         <h3 className={styles.choose}>Choose the size</h3>
         <div className={styles.sizes}>
           <div className={styles.size} onClick={() => setSize(0)}>
-            <Image src="/image/size.png" alt="" layout="fill" />
+            <Image src="/image/size.png" layout="fill" alt="" />
             <span className={styles.number}>Small</span>
           </div>
           <div className={styles.size} onClick={() => setSize(1)}>
-            <Image src="/image/size.png" alt="" layout="fill" />
+            <Image src="/image/size.png" layout="fill" alt="" />
             <span className={styles.number}>Medium</span>
           </div>
           <div className={styles.size} onClick={() => setSize(2)}>
-            <Image src="/image/size.png" alt="" layout="fill" />
+            <Image src="/image/size.png" layout="fill" alt="" />
             <span className={styles.number}>Large</span>
           </div>
         </div>
@@ -49,25 +49,31 @@ const Product = () => {
               className={styles.checkbox}
             />
             <label htmlFor="double">Double Ingredients</label>
+          </div>
+          <div className={styles.option}>
             <input
+              className={styles.checkbox}
               type="checkbox"
               id="cheese"
               name="cheese"
-              className={styles.checkbox}
             />
             <label htmlFor="cheese">Extra Cheese</label>
+          </div>
+          <div className={styles.option}>
             <input
+              className={styles.checkbox}
               type="checkbox"
               id="spicy"
               name="spicy"
-              className={styles.checkbox}
             />
             <label htmlFor="spicy">Spicy Sauce</label>
+          </div>
+          <div className={styles.option}>
             <input
+              className={styles.checkbox}
               type="checkbox"
               id="garlic"
               name="garlic"
-              className={styles.checkbox}
             />
             <label htmlFor="garlic">Garlic Sauce</label>
           </div>
